dd9141f78fcbdac1a6757eaa6551e1c28ad8088a
sgr-ksmt <melodydance.k.s@gmail.com>
2016-11-15 10:51:17 +0900

    setup project

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Suguru Kishimoto on 11/15/16.
+//  Copyright © 2016 Suguru Kishimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
