f1a61b2911a636b32eddb1fadb6d4ba5c09100a4
sgr-ksmt <melodydance.k.s@gmail.com>
2016-11-16 10:56:42 +0900

    add PullToDismiss codes

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import PullToDismiss
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.        PullToDismiss(
     }
 
     override func didReceiveMemoryWarning() {
